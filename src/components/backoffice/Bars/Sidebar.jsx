1a4cddbdb68ef84fd1e41239f57541ee14c34f15
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-05-16 17:06:24 -0300

    Refactor Sidebar component to use dynamic active state and update navigation links

--- src/components/backoffice/Bars/Sidebar.jsx
+++ src/components/backoffice/Bars/Sidebar.jsx
@@ -8,19 +8,66 @@ import { cn } from "@/src/lib/cn";
 import {
   LayoutGrid,
   Users,
-  User,
+  ContactRound,
   Settings,
+  SquareUser,
   Truck,
-  Wheat,
   Warehouse,
-  Cloudy,
+  ExternalLink,
   Folder,
   FolderOpen
 } from "lucide-react";
-import { useTheme } from "next-themes";
+
 const Sidebar = () => {
   const [active, setActive] = useState(1);
-
+  const sidebarLinks = [
+    {
+      title: "Dashboard",
+      icon: LayoutGrid ,
+      href:"/dashboard"
+    },
+    {
+      title: "Catalogue",
+      icon: Folder ,
+      href:"/dashboard/catalogue"
+      
+    },
+    {
+      title: "Customers",
+      icon: Users ,
+      href: "/dashboard/customers"
+    },
+    {
+      title: "Markets",
+      icon: Warehouse,
+      href:"/dashboard/markets"
+    },
+    {
+      title: "Orders",
+      icon: Truck ,
+      href:"/dashboard/orders"
+    },
+    {
+      title: "Farmers",
+      icon: SquareUser,
+      href:"/dashboard/farmers"
+    },{
+      title: "Staff",
+      icon: ContactRound,
+      href:"/dashboard/staff"
+    },
+    {
+      title: "Settings",
+      icon: Settings ,
+      href:"/dashboard/settings"
+    },
+    ,
+    {
+      title: "Online Store",
+      icon: ExternalLink ,
+      href:"/dashboard/online_store"
+    },
+  ]
   return (
     <div className="bg-slate-50 dark:bg-slate-700 space-y-6 w-64 h-screen text-emerald-700 dark:text-slate-50 shadow-md px-6 py-4 fixed left-0 top-0 z-42 ">
       <Link className="" href="#">
@@ -32,110 +79,27 @@ const Sidebar = () => {
       </Link>
 
       <div className="space-y-3 flex flex-col mt-14">
-        <button onClick={() => setActive(1)}>
-          <Link
-            href="/dashboard"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 1,
-            })}
-          >
-            <LayoutGrid />
-            <span>Dashboard</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(2)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 2,
-            })}
-          >
-            {
-              active === 2 ? ( <FolderOpen /> ) : ( <Folder /> )
-            }
-            <span>Catalogue</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(3)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 3,
-            })}
-          >
-            <Users />
-            <span>Customers</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(4)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 4,
-            })}
-          >
-            <Warehouse />
-            <span>Markets</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(5)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 5,
-            })}
-          >
-            <Wheat />
-            <span>Farmers</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(6)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 6,
-            })}
-          >
-            <Truck />
-            <span>Orders</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(7)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 7,
-            })}
-          >
-            <User />
-            <span>Staff</span>
-          </Link>
-        </button>
-        <button onClick={() => setActive(8)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 8,
-            })}
-          >
-            <Settings className={cn("", {
-              "animate-[spin_0.5s_ease-in-out]" : active === 8
-            })}/>
-            <span>Settings</span>
-          </Link>
-        </button>
+        {
+          sidebarLinks.map((item, i)=> {
+            const Icon = item.icon
+            return(
+              <Link
+                onClick={()=>setActive(i)}
+                key={i}
+                  href={item.href}
+                  className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
+                    "border-green-600": active === i,
+                    "" : item.title === "settings"
+                  })}
+                >
+                  <Icon />
+                  <span>{item.title}</span>
+              </Link>
+            );
+          })
+          }
 
-        <button onClick={() => setActive(9)}>
-          <Link
-            href="#"
-            className={cn("flex items-center space-x-2 px-6 py-2 border-l-4", {
-              "border-green-600": active === 9,
-            })}
-          >
-            <Cloudy />
-            <span>Online store</span>
-          </Link>
-        </button>
+        
       </div>
     </div>
   );
