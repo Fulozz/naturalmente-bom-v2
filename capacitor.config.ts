e0c36dca3a4121b28881d187f82dd75d5cd3b60d
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-06-01 17:27:32 -0300

    Refactor Sidebar component to use dynamic active state and remove unnecessary CSS class

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.naturalmentebom.app',
-  appName: 'android',
-  webDir: 'public'
-};
-
-export default config;
